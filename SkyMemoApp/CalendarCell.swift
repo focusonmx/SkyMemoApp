76716f8200a90a26dc691f9ad553146b322209d9
simyoju <dawnofspring@daum.net>
2021-09-23 14:17:25 +0900

    add CalendarHelper.swift and CalendarCell.swift

---
+++ SkyMemoApp/CalendarCell.swift
@@ -0,0 +1,12 @@
+//
+//  CalendarCell.swift
+//  SkyMemoApp
+//
+//  Created by simyo on 2021/09/23.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    @IBOutlet var dayOfMonthLabel: UILabel
+}
--- SkyMemoApp/CalendarViewController.swift
+++ SkyMemoApp/CalendarViewController.swift
@@ -8,24 +8,41 @@
 import UIKit
 
 class CalendarViewController: UIViewController {
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let selectedDate = Date()
+    let totalSquares = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("제발")
-        print("되라되라ㅏㅏㅏ")
-
-        // Do any additional setup after loading the view.
+        setCellsView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setCellsView(){
+        let width = (collectionView.frame.size.width - 2) / 8
+        let height = (collectionView.frame.size.height - 2) / 8
+        
+        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.itemSize = CGSize(width: width, height: height)
     }
-    */
+    
+    @IBAction func prevMonth(_ sender: Any) {
+        
+    }
+    
+    @IBAction func nextMonth(_ sender: Any) {
+    }
+    
+}
 
+extension CalendarViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return totalSquares.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CalendarCell", for: indexPath) as! CalendarCell
+        cell.dayOfMonthLabel.text = totalSquares[indexPath]
+    }
 }
