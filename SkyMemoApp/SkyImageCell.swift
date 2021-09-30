9117ebfe8ce38541c8c73d78866b68bf00343d35
yujinlee <yj587878@gmail.com>
2021-10-01 00:14:39 +0900

    Semi Complete View

---
+++ SkyMemoApp/SkyImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  SkyImageCollectionViewCell.swift
+//  SkyMemoApp
+//
+//  Created by 이유진 on 2021/09/28.
+//
+
+import UIKit
+
+class SkyImageCell: UICollectionViewCell {
+    
+    @IBOutlet weak var skyImgView: UIImageView!
+    
+    
+}
+
