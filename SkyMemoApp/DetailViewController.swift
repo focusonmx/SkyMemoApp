56cff462ca11ae305961d211376d12cd32aed26c
simyoju <dawnofspring@daum.net>
2021-10-06 11:20:39 +0900

    [conflict 해결] 전전단계로 commit 돌리고, 스토리보드 삭제된 상태에서 프로젝트 파일 열리게 함

--- SkyMemoApp/DetailViewController.swift
+++ SkyMemoApp/DetailViewController.swift
@@ -8,22 +8,77 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var skyCollectionView: UICollectionView!
+    
+    @IBOutlet weak var memoLabel: UILabel!
 
+    @IBOutlet weak var pageControl: UIPageControl!
+    //이미지 배열
+    let imgArray: Array<UIImage> = [UIImage(named: "SampleSky1.png")!,UIImage(named: "SampleSky2.png")!,UIImage(named: "SampleSky3.png")!]
+    //메모 배열
+    let memoArray: [String] = ["오늘하늘 너무 이쁘다 >_<","구름 안뇽","알바 끝나고 가는길"]
+
+    
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        skyCollectionView.delegate = self
+        skyCollectionView.dataSource = self
 
-        // Do any additional setup after loading the view.
+        memoLabel.text = memoArray[0]
+        
+        pageControl.numberOfPages = 3
+        
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension DetailViewController: UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int)->Int{
+        return imgArray.count
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = skyCollectionView.dequeueReusableCell(withReuseIdentifier: "SkyImageCell", for: indexPath) as! SkyImageCell
+        cell.skyImgView.image = imgArray[indexPath.row]
+        cell.skyImgView.layer.masksToBounds = true
+        cell.skyImgView.layer.cornerRadius = cell.skyImgView.frame.width/2
+        cell.skyImgView.clipsToBounds = true
+        cell.skyImgView.layer.borderColor = CGColor(red: 191.0/255, green: 232.0/255, blue: 255.0/255, alpha: 1.0)
+        cell.skyImgView.layer.borderWidth = 5
+
+
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath)-> CGSize {
+        return CGSize(width: skyCollectionView.frame.size.width , height: skyCollectionView.frame.width)
+    }
+    
+    
+    
 
 }
+
+//스크롤 됐을 때 페이지컨트롤 변경되고 글씨 변경
+extension DetailViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView){
+        let width = scrollView.bounds.size.width
+        let x = scrollView.contentOffset.x + (width / 2.0)
+        
+        let newPage = Int(x/width)
+        if pageControl.currentPage != newPage {
+            pageControl.currentPage = newPage
+        }
+        
+        memoLabel.text = memoArray[newPage]
+    }
+}
+
+
+
--- SkyMemoApp/EditViewController.swift
+++ SkyMemoApp/EditViewController.swift
@@ -9,21 +9,142 @@ import UIKit
 
 class EditViewController: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var removeButton: UIButton!
+    
+    @IBOutlet weak var doneButton: UIButton!
+    
+    let picker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        dateLabel.text="210903"
+        textField.text = "오늘하늘 너무 이쁘다 >_<"
+        
+        picker.delegate = self
+        textField.delegate = self
+   
+        makeImageView()
+        setKeyboardUpAndDown()
+        
     }
     
 
-    /*
-    // MARK: - Navigation
+    func makeImageView() {
+        //이미지뷰를 동그랗게 만들고 처음 이미지 설정
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = imageView.frame.width/2
+        imageView.clipsToBounds = true
+        imageView.layer.borderColor = CGColor(red: 191.0/255, green: 232.0/255, blue: 255.0/255, alpha: 1.0)
+        imageView.layer.borderWidth = 5
+        imageView.image = UIImage(named: "SampleSky1.png")
+        imageView.contentMode = .scaleAspectFill
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        //이미지뷰에 클릭이벤트 추가
+        let tapGR = UITapGestureRecognizer(target:self, action: #selector(self.imageTapped))
+        imageView.addGestureRecognizer(tapGR)
+        imageView.isUserInteractionEnabled = true
+        
+        
     }
-    */
+    
+    func setKeyboardUpAndDown() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+       
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(endEditing)))
+        
+    }
+    
+    //이미지뷰 탭했을 때 UIImagePicker 띄우기
+    @objc func imageTapped(sender: UITapGestureRecognizer) {
+        if sender.state == .ended{
+            
+            let alert = UIAlertController(title: "하늘사진 수정하기",message: "", preferredStyle: .actionSheet)
+            let library = UIAlertAction(title: "앨범에서 가져오기", style:.default){ (action) in self.openLibrary()}
+            let camera = UIAlertAction(title: "카메라로 촬영하기", style: .default){ (action) in self.openCamera()}
+            
+            let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+            alert.addAction(library)
+            alert.addAction(camera)
+            alert.addAction(cancel)
+            
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    //갤러리에서 사진 가져오기
+    func openLibrary() {
+        picker.sourceType = .photoLibrary
+        present(picker, animated: false, completion: nil)
+    }
+    
+    //카메라에서 찍어서 가져오기
+    func openCamera() {
+        
+        if(UIImagePickerController.isSourceTypeAvailable(.camera)){
+            picker.sourceType = .camera
+            present(picker, animated: false, completion: nil)
+            
+        }
+        else{
+            print("Camera not available")
+        }
+        
+        
+    }
+    
+    @objc func keyboardWillShow(_ sender:Notification){
+        self.view.frame.origin.y = -180
+    }
+    
+    @objc func keyboardWillHide(_ sender:Notification){
+        self.view.frame.origin.y = 0
+    }
+    
+    @objc func endEditing(){
+        textField.resignFirstResponder()
+    }
 
+}
+
+extension EditViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //텍스트 필드 최대 문자수 제한
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            if (textField.text?.count)! + string.count > 20 {
+                
+                return false
+            } else {
+                return true
+            }
+    
+    }
+}
+
+extension EditViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            
+            imageView.image = image
+            
+            print(info)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+ 
 }
